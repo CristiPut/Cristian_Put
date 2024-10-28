dfc63ff272d358212ab2542794a57bf8bbce7455
CristiPut <putcristian14@gmail.com>
2024-10-28 21:06:08 +0200

    Add files via upload

--- tema 3.py
+++ tema 3.py
@@ -15,7 +15,7 @@ cati_papanasi_s_au_comandat = comenzi.count("papanasi")
 pret_papanasi = preturi[0][1]
 castig_papanasi = cati_papanasi_erau_la_inceput * pret_papanasi
 cat_gulas_era_la_inceput = meniu.count("guias")
-cat_gluas_s_a_comandat = comenzi.count("guias")
+cat_gulas_s_a_comandat = comenzi.count("guias")
 pret_guias = preturi[2][1]
 castig_gulas = cat_gulas_era_la_inceput * pret_guias
 while studenti :
@@ -24,5 +24,17 @@ while studenti :
     print(f" Studentul {studentul} a comandat {comanda}")
     tavi.pop()
     istoric_comenzi.append([studentul, comanda])
-print(f"S-au comandat {cati_papanasi_s_au_comandat} papanasi, {cata_ceafa_s_a_comandat} ceafa, {cat_gluas_s_a_comandat} guias ")
-print(f"Mai sunt {len(tavi)} tavi")
+print(f"S-au comandat {cati_papanasi_s_au_comandat} papanasi, {cata_ceafa_s_a_comandat} ceafa, {cat_gulas_s_a_comandat} guias ")
+print(f"Mai sunt {len(tavi)} tavi.")
+stoc_ramas= {"papanasi": meniu.count("papanasi"), "ceafa": meniu.count("ceafa"), "guias": meniu.count("guias")}
+print(f"Mai este ceafa: {stoc_ramas['ceafa'] > 0}.")
+print(f"Mai sunt papanasi: {stoc_ramas['papanasi'] > 0}.")
+print(f"Mai sunt guias: {stoc_ramas['guias'] > 0}.")
+total_incasat = 0
+for  comanda in istoric_comenzi:
+    for produs in preturi:
+        if comanda == produs[0]:
+            total_incasat += produs[1]
+print(f"Cantina a încasat: {total_incasat} lei.")
+produse_disponibile = [produs for produs in preturi if produs[1] <= 7]
+print(f"Produse care costă cel mult 7 lei: {produse_disponibile}.")
